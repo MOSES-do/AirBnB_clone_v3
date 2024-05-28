5539c6359b0eafc7afdaa7260d3d5221f5eae07b
Esumei Moses <archer.me24@gmail.com>
2024-05-28 13:04:04 -0700

    state route updates

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -6,7 +6,7 @@ from models.state import State
 from api.v1.views import app_views
 
 
-@app_views.route('/states', methods=['GET'], strict_slashes=False)
+@app_views.route('/states', methods=['GET'], strict_slashes=False, endpoint='all_state')
 def all_states():
     """get all states from strage"""
     all_states = []
@@ -16,7 +16,27 @@ def all_states():
     return jsonify(all_states)
 
 
-@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
+@app_views.route("/states", methods=["POST"], strict_slashes=False, endpoint='state_create')
+def state_create():
+    """
+    create state route
+    :return: newly created state obj
+    """
+    state_json = request.get_json(silent=True)
+    if state_json is None:
+        abort(400, 'Not a JSON')
+    if "name" not in state_json:
+        abort(400, 'Missing name')
+
+    new_state = State(**state_json)
+    new_state.save()
+    res = jsonify(new_state.to_dict())
+    res.status_code = 201
+
+    return res
+
+
+@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False, endpoint='single_state')
 def single_state(state_id):
     """return state based on id"""
     s = storage.all(State)
@@ -27,19 +47,8 @@ def single_state(state_id):
             abort(404, description="State not found")
 
 
-@app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
-def single_state(state_id):
-    """delete state based on id"""
-    entity = storage.get(State, str(state_id))
-    if entity is None:
-        abort(404, description="State not found")
-    storage.delete(entity)
-    storage.save()
-    return jsonify({})
-
-
-@app_views.route("/states/<state_id>",  methods=["PUT"], strict_slashes=False)
-def state_put(state_id):
+@app_views.route("/states/<state_id>", methods=["PUT"], strict_slashes=False, endpoint='update_state')
+def update_state(state_id):
     """
     updates specific State object by ID
     return: state object and 200 on success, or 400 or 404 on failure
@@ -57,3 +66,16 @@ def state_put(state_id):
         """
         if key not in ["id", "created_at", "updated_at"]:
             setattr(found_obj, key, val)
+
+
+@app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False, endpoint='del_state')
+def del_state(state_id):
+    """delete state based on id"""
+    entity = storage.get(State, str(state_id))
+    if entity is None:
+        abort(404, description="State not found")
+    storage.delete(entity)
+    storage.save()
+    return jsonify({})
+
+
