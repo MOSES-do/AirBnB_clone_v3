f00dc956abd230b7a0caea5bfc66bcff40b85966
Esumei Moses <archer.me24@gmail.com>
2024-05-27 21:47:29 -0700

    state route updated

--- api/v1/views/state.py
+++ api/v1/views/state.py
@@ -6,15 +6,6 @@ from models.state import State
 from api.v1.views import app_views
 
 
-#!/usr/bin/python3
-"""
-route for handling State objects
-"""
-from flask import jsonify, abort, request
-from api.v1.views import app_views, storage
-from models.state import State
-
-
 @app_views.route("/states", methods=["GET"], strict_slashes=False)
 def state_get_all():
     """
